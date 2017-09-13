6900fa74dd2f22f12e80a2cb4cf9803ff3c639f9
YangMaoMao <15828351114@163.com>
2017-09-13 21:43:45 +0800

    server

--- src/com/tomcat/startup/HttpServer.java
+++ src/com/tomcat/startup/HttpServer.java
@@ -1,8 +1,14 @@
 package com.tomcat.startup;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.tomcat.request.Request;
+import com.tomcat.response.Response;
 
 public class HttpServer {
 	
@@ -19,7 +25,40 @@ public class HttpServer {
 		try {
 			serverSocket = new ServerSocket(port, 0, InetAddress.getByName("127.0.0.1"));
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.exit(1);
+		}
+		
+		while(!shutdown){
+			Socket socket = null;
+			InputStream input = null;
+			OutputStream output = null;
+			
+			try {
+				socket = serverSocket.accept();
+				input = socket.getInputStream();
+				output = socket.getOutputStream();
+				
+				//创建请求对象
+				Request request = new Request(input);
+				request.parse();
+				
+				//创建响应对象
+				Response response = new Response(request);
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
 		}
 	}
 
