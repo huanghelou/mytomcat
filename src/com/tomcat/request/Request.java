36492868d8429815c3e924a8ad09f7d66c5b839d
YangMaoMao <15828351114@163.com>
2017-09-13 11:27:56 +0800

    Response类修改

--- src/com/tomcat/request/Request.java
+++ src/com/tomcat/request/Request.java
@@ -1,5 +1,6 @@
 package com.tomcat.request;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 public class Request {
@@ -11,14 +12,31 @@ public class Request {
 		this.input = input;
 	}
 	
-	public void parse(){
+	public void parse() throws IOException{
 		
 		StringBuffer request = new StringBuffer(2048);
-		int i;
 		byte[] buffer = new byte[2048];
+		while(input.read(buffer) != -1){
+			request.append(new String(buffer));
+		}
+		System.out.println(request);
+		uri = parseUri(request.toString());
 	}
 
+	/**
+	 * 截取请求信息中2个空格之间的内容
+	 * @param requestString
+	 * @return
+	 */
 	private String parseUri(String requestString){
+		int index1, index2;
+		index1 = requestString.indexOf(' ');
+		if(index1 != -1){
+			index2 = requestString.indexOf(' ', index1 + 1);
+			if(index2 > index1){				
+				return requestString.substring(index1 + 1, index2);
+			}
+		}
 		return null;
 	}
 	
